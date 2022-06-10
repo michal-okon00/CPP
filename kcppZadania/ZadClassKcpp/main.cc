31cf534e52e25980c33ed093f1fa740e14b98258
Michał Okoń <95043997+michal-okon00@users.noreply.github.com>
2022-06-10 18:17:44 +0200

    Update main.cc

--- kcppZadania/ZadClassKcpp/main.cc
+++ kcppZadania/ZadClassKcpp/main.cc
@@ -10,11 +10,11 @@ int main(){
 	    cout<<"============================"<<endl;
     	cout<<"Wybierz zadanie do wykonania"<<endl;
     	cout<<"----------------------------"<<endl;
-    	cout<<"1) Sprawdzanie parzystosci za pomoca operatora warunkowego [4.3]"<<endl;
-    	cout<<"2) Zwracanie przez wartosc [2.1]"<<endl;
-    	cout<<"3) Arytmetyka wskaznikow [3.3]"<<endl;
-    	cout<<"4) Przyklad manipulacji strumieniem cout [4.0]"<<endl;
-    	cout<<"5) Przykladowe roznice miedzy C a C++ [4.2]"<<endl;
+    	cout<<"1) Sprawdzanie parzystosci za pomoca operatora warunkowego [4.3]"<<endl; //Operacje wejscia/wyjscia
+    	cout<<"2) Zwracanie przez wartosc [2.1]"<<endl; //Zwracanie przez funkcje
+    	cout<<"3) Arytmetyka wskaznikow [3.3]"<<endl; //Arytmetyka wkaznikow
+    	cout<<"4) Przyklad manipulacji strumieniem cout [4.0]"<<endl; //Operacje wejscia/wyjscia
+    	cout<<"5) Przykladowe roznice miedzy C a C++ [4.2]"<<endl; //Operacje wejscia/wyjscia
     	cout<<"Inne) Wyjdz z programu"<<endl;
     	int option;
     	cin>>option;
